2f886f7ec9faa2f3969b3d6ece64062209d6ac30
jinygod <ayclub8866@naver.com>
2024-07-26 13:42:51 +0900

    style: 초히든번호 firework 효과 다시 추가

--- src/components/LotteryNumberList.js
+++ src/components/LotteryNumberList.js
@@ -1,7 +1,16 @@
 import React from "react";
 import getClassname from "../utils/getClassname";
+import { useEffect } from "react";
+import { firework } from "../utils/firework";
 
 const LotteryNumberList = ({ numbers, isHighlight, isUltraHighlight }) => {
+
+  useEffect(() => {
+    if (isUltraHighlight) {
+      firework(); // isUltraHighlight가 true일 때 firework() 호출
+    }
+  }, [isUltraHighlight]); // isUltraHighlight가 변경될 때마다 호출
+
   return (
     <div
       className={`numberlist ${
