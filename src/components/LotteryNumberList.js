91d288fafcd930b063a1887e40806a9a2da3f82d
jinygod <ayclub8866@naver.com>
2024-08-01 13:32:18 +0900

    feat: numberlist 클릭시 클립보드 복사 이벤트 추가

--- src/components/LotteryNumberList.js
+++ src/components/LotteryNumberList.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import getClassname from "../utils/getClassname";
-import { useEffect } from "react";
 import { firework } from "../utils/firework";
 
 const LotteryNumberList = ({ numbers, isHighlight, isUltraHighlight }) => {
+  const [copySuccess, setCopySuccess] = useState(false);
 
   useEffect(() => {
     if (isUltraHighlight) {
@@ -11,11 +11,27 @@ const LotteryNumberList = ({ numbers, isHighlight, isUltraHighlight }) => {
     }
   }, [isUltraHighlight]); // isUltraHighlight가 변경될 때마다 호출
 
+  const copyToClipboard = () => {
+    const numbersString = numbers.join(",");
+    navigator.clipboard
+      .writeText(numbersString)
+      .then(() => {
+        setCopySuccess(true);
+        setTimeout(() => {
+          setCopySuccess(false);
+        }, 2000); // 2초 후에 메시지 숨기기
+      })
+      .catch((err) => {
+        console.error("클립보드 복사 실패: ", err);
+      });
+  };
+
   return (
     <div
       className={`numberlist ${
         isUltraHighlight ? "ultra-highlight" : isHighlight ? "highlight" : ""
       }`}
+      onClick={copyToClipboard} // 박스 전체 클릭 시 복사
     >
       {isUltraHighlight && (
         <div className="highlight-message">초히든번호!!!!!!</div>
@@ -28,6 +44,11 @@ const LotteryNumberList = ({ numbers, isHighlight, isUltraHighlight }) => {
           {number}
         </span>
       ))}
+      {copySuccess && (
+        <div className="copy-success-message">
+          번호가 클립보드에 복사되었습니다.
+        </div>
+      )}
     </div>
   );
 };
